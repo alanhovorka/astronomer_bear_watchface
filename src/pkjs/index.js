1d024dadc343e563099f3181f81b4d32ea431712
Alan Hovorka <alanhovorka94@gmail.com>
2017-12-22 16:47:48 -0500

    Got weather service working

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,31 +1,63 @@
-// Listen for when the watchface is opened
-Pebble.addEventListener('ready', 
-  function(e) {
-    console.log('PebbleKit JS ready!');
-  }
-);
+var myAPIKey = '';
 
-// Listen for when an AppMessage is received
-Pebble.addEventListener('appmessage',
-  function(e) {
-    console.log('AppMessage received!');
-    getWeather();
-  }                     
-);
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
 
 function locationSuccess(pos) {
-  //We will request weather here
+  // Construct URL
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+      console.log(url);
+      console.log(responseText);
+      
+      // Temperature in Kelvin requires adjustment
+      var temperature = Math.round(json.main.temp - 273.15);
+      console.log('Temperature is ' + temperature);
+
+      // Conditions
+      var conditions = json.weather[0].main;      
+      console.log('Conditions are ' + conditions);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        'TEMPERATURE': temperature,
+        'CONDITIONS': conditions
+      };
+      
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log('Weather info sent to Pebble successfully');
+        },
+        function(e) {
+          console.log('Error sending weather info to Pebble');
+        }
+      );
+    }
+  );
 }
 
 function locationError(err) {
   console.log('Error requesting location');
 }
 
-function getWeather(err) {
-  navigator.geolocation.getcurrentPosition(
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
     locationSuccess,
     locationError,
-    {timeout: 15000, maxiumAge: 60000}
+    {timeout: 15000, maximumAge: 60000}
   );
 }
 
@@ -39,52 +71,18 @@ Pebble.addEventListener('ready',
   }
 );           
 
-var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    callback(this.responseText);
-  };
-  xhr.open(type, url);
-  xhr.send();
-};
-
-var loadConfig = require('./config.js');
-var myAPIKey = loadConfig.weatherKey;
-
-function locationSuccess(pos) {
-  // Construct URL
-  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
-
-  // Send request to OpenWeatherMap
-  xhrRequest(url, 'GET', 
-    function(responseText) {
-      // responseText contains a JSON object with weather info
-      var json = JSON.parse(responseText);
-
-      // Temperature in Kelvin requires adjustment
-      var temperature = Math.round(json.main.temp - 273.15);
-      console.log('Temperature is ' + temperature);
-
-      // Conditions
-      var conditions = json.weather[0].main;      
-      console.log('Conditions are ' + conditions);
-    }      
-  );
-}
-
-// Assemble dictionary using our keys
-var dictionary = {
-  'TEMPERATURE': temperature,
-  'CONDITIONS': conditions
-};
-
-// Send to Pebble
-Pebble.sendAppMessage(dictionary,
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
   function(e) {
-    console.log('Weather info sent to Pebble successfully');
-  },
-  function(e) {
-    console.log('Error sending weather info to Pebble');
+    console.log('PebbleKit JS ready!');
+    getWeather();    
   }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log('AppMessage received!');
+    getWeather();
+  }                     
 );
