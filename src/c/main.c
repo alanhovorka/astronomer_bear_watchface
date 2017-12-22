72fa55c1d2caf0603f57d6a504c73a55a4458ad2
Alan Hovorka <alanhovorka94@gmail.com>
2017-12-22 14:05:52 -0500

    Added AppMessage and prepared for PebbleKit JS

--- src/c/main.c
+++ src/c/main.c
@@ -78,6 +78,23 @@ static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_weather_font);
 }
 
+//weather service callbacks
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void init() {
   // Create main Window element and assign to point
   s_main_window = window_create();
@@ -95,6 +112,16 @@ static void init() {
   
   // Make sure the time is displayed from the start
   update_time();
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  // Open AppMessage
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
 }
 
 static void deinit() {
