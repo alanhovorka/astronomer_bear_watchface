3064f61eb6187197ec55a2a5b791653f1ba76e06
Alan Hovorka <alanhovorka94@gmail.com>
2017-12-22 14:57:17 -0500

    Getting ready to feed weather api key in

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -11,3 +11,60 @@ Pebble.addEventListener('appmessage',
     console.log('AppMessage received!');
   }                     
 );
+
+function locationSuccess(pos) {
+  //We will request weather here
+}
+
+function locationError(err) {
+  console.log('Error requesting location');
+}
+
+function getWeather(err) {
+  navigator.geolocation.getcurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maxiumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened and then get the weather
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log('PebbleKit JS ready');
+    
+    // Get the initial weather
+    getWeather();
+  }
+);           
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+      pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + myAPIKey;
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      var temperature = Math.round(json.main.temp - 273.15);
+      console.log('Temperature is ' + temperature);
+
+      // Conditions
+      var conditions = json.weather[0].main;      
+      console.log('Conditions are ' + conditions);
+    }      
+  );
+}
